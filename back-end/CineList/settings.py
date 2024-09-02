ea33526a0ff6fad5e27f8cee3cf1a3cadde6b0ff
kcezario <102490598+kcezario@users.noreply.github.com>
2024-09-02 13:03:09 -0300

    ajustando deploy - 13

--- back-end/CineList/settings.py
+++ back-end/CineList/settings.py
@@ -156,6 +156,9 @@ TMDB_API_KEY = config('TMDB_API_KEY')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
