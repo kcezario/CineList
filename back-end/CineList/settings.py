ab0fc4ee780771708b1b59fb72312d60b523b190
kcezario <102490598+kcezario@users.noreply.github.com>
2024-09-02 12:26:17 -0300

    ajustando deploy - 10

--- back-end/CineList/settings.py
+++ back-end/CineList/settings.py
@@ -156,9 +156,8 @@ TMDB_API_KEY = config('TMDB_API_KEY')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
--- back-end/build_files.sh
+++ back-end/build_files.sh
@@ -2,5 +2,4 @@ python3.12 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
-python movies/tools.py
 python manage.py collectstatic --noinput
